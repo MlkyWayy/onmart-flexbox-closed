c8c2b1204c73c1647f1e0aee60f7fae70065a000
Fajarr-M <61728959+Fajarr-M@users.noreply.github.com>
2021-01-15 14:01:57 +0700

    Update script.js

--- Project latihan/script.js
+++ Project latihan/script.js
@@ -1,12 +1,27 @@
 const hai = document.querySelector(".hero p");
-const iAja = document.querySelectorAll(".kat-isi");
+const kat = document.querySelectorAll(".kat-isi");
+const buy = document.querySelectorAll("button");
+const botNav = document.querySelectorAll(".container-setting li")
 
 hai.addEventListener("click",function(){
     alert("Coming Soon");
 });
 
-for(let i = 0; i < iAja.length; i++){
-    iAja[i].addEventListener("click",function(){
+for(let i = 0; i < kat.length; i++){
+    kat[i].addEventListener("click",function(){
     alert("Coming Soon");
 });
 }
+
+for(let i = 0; i < buy.length; i++){
+    buy[i].addEventListener("click",function(){
+    alert("Coming Soon");
+});
+}
+
+for(let i = 0; i < 3; i++){
+    botNav[i].addEventListener("click",function(){
+    alert("In Progress");
+});
+}
+
